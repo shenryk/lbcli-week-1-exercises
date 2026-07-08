5862ec87efc7931d9f5dab21fb3cf562578a12dd
shenku <senhen123@gmail.com>
2026-07-09 01:54:01 +0300

    made ko more changes

--- submission/03.sh
+++ submission/03.sh
@@ -1,2 +1,2 @@
 # Write the bitcoin cli command to get the bitcoin node latest block height 
-bitcoin-cli getblockcount
+bitcoin-cli -regtest getblockcount
--- submission/04.sh
+++ submission/04.sh
@@ -1,4 +1,4 @@
 # Create a wallet with the name "builderswallet".
-bitcoin-cli createwallet "builderswallet"
+bitcoin-cli -regtest createwallet "builderswallet"
 #lets see if this works 
 #test this 
