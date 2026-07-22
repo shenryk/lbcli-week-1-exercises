bef3e1a8b39b836bbd86a4baec4639299acad0cf
shenku <senhen123@gmail.com>
2026-07-22 23:24:59 +0300

    made some changes to script 2

--- submission/02.sh
+++ submission/02.sh
@@ -1,7 +1,7 @@
 # Write the bitcoin cli command to get the bitcoin node network name
 #bitcoin-cli -regtest  getblockchaininfo | jq -r '.chain'
 
-bitcoin-cli getblockchaininfo | jq -r '.chain'
+bitcoin-cli -regtest getblockchaininfo | jq -r '.chain'
 
 
 
